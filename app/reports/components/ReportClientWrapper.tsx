0b4f83c539b300dc2082b9dafa4f06a83effe79b
JarvisCamper <kharelramit@gmail.com>
2025-12-02 17:06:34 +0545

    redun

--- app/reports/components/ReportClientWrapper.tsx
+++ app/reports/components/ReportClientWrapper.tsx
@@ -4,6 +4,7 @@ import dynamic from "next/dynamic";
 import React from "react";
 
 const ReportDashboard = dynamic(() => import("./ReportDashboard"), { ssr: false });
+console.log("hi")
 
 export default function ReportClientWrapper() {
   return <ReportDashboard />;
